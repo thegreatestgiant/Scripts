883844ba339e2ad6ae895e103f482922e266faea
The Greatest Giant <seanalter1@gmail.com>
2023-03-13 20:36:47 -0400

    Update Base.sh

--- Base.sh
+++ Base.sh
@@ -1,19 +1,27 @@
 #!/bin/bash -i
 
-shopt -s expand_aliases
+function setup_shell() {
+    echo "alias bat='batcat'
+    alias cmatrix='cmatrix -sb'
+    alias i='sudo apt install -y'
+    alias cd..='cd ..'
+    alias ip='curl icanhazip.com'
+    alias ls='ls --color=auto -h'
+    alias speedtest='curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python3 -'
+    alias weather='curl wttr.in'
+    alias zupdate='sudo apt update && sudo apt upgrade -y'
+    HISTTIMEFORMAT='%Y-%m-%d %T '" >> ~/.bashrc
 
-alias bat='batcat'
-alias cmatrix='cmatrix -sb'
-alias i='sudo apt install -y'
-alias cd..='cd ..'
-alias ip='curl icanhazip.com'
-alias ls='ls --color=auto -h'
-alias speedtest='curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python3 -'
-alias weather='curl wttr.in'
-alias zupdate='sudo apt update && sudo apt upgrade -y'
+    exec bash
+}
 
-sudo apt update -y && sudo apt upgrade -y
+function setup_packages() {
+    sudo apt update -y && sudo apt upgrade -y
 
-sudo apt install -y nano net-tools docker docker.io docker-compose nautilus bat nginx sshfs zip unzip cmatrix tree
+    sudo apt install -y nano net-tools docker docker.io docker-compose nautilus bat nginx sshfs zip unzip cmatrix tree
 
-echo "Done!"
+    echo "Done!"
+}
+
+setup_packages
+setup_shell
