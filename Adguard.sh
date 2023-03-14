fca13edf15b9492492397bbb82110178f1827ceb
The Greatest Giant <seanalter1@gmail.com>
2023-03-14 11:45:28 -0400

    Update Adguard.sh

--- Adguard.sh
+++ Adguard.sh
@@ -5,8 +5,6 @@ curl -sSL https://cdn.jsdelivr.net/gh/thegreatestgiant/cdn@latest/Base.sh | sh -
 
 # Kill 80, 53
 sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
-sudo lsof -t -i tcp:53 -s tcp:listen | sudo xargs kill
-sudo lsof -t -i udp:53 -s udp:listen | sudo xargs kill
 
 echo "Starting docker"
 echo "version: '2'
@@ -15,8 +13,8 @@ services:
     image: adguard/adguardhome
     container_name: adguardhome
     ports:
-      - 53:53/tcp
-      - 53:53/udp
+      - 54:53/tcp
+      - 54:53/udp
       - 784:784/udp
       - 853:853/tcp
       - 3000:3000/tcp
