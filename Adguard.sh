1c0a2d7d7559ea2a3eae6128ff17b5b3715bea7e
The Greatest Giant <seanalter1@gmail.com>
2023-03-14 11:24:15 -0400

    Create Adguard.sh

---
+++ Adguard.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -i
+
+# Base
+curl -sSL https://cdn.jsdelivr.net/gh/thegreatestgiant/cdn@latest/Base.sh | sh -s
+
+echo "Starting docker"
+echo "version: '2'
+services:
+  adguardhome:
+    image: adguard/adguardhome
+    container_name: adguardhome
+    ports:
+      - 53:53/tcp
+      - 53:53/udp
+      - 784:784/udp
+      - 853:853/tcp
+      - 3000:3000/tcp
+      - 80:80/tcp
+      - 443:443/tcp
+    volumes:
+      - ./workdir:/opt/adguardhome/work
+      - ./confdir:/opt/adguardhome/conf
+    restart: unless-stopped" > docker-compose.yml
+    
+    echo "Made file"
+    
+    echo "docker-compose up -d"
+    docker-compose up -d
+    
+    echo "finished"
+    
+    echo "go to ${hostname -I}:3000"
+    echo "Done!"
