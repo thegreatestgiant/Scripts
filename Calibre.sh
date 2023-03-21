f3a31dbaaeb0a40655fe8aceead784f107aa0ad2
The Greatest Giant <seanalter1@gmail.com>
2023-03-21 12:58:57 -0400

    Update Calibre.sh

--- Calibre.sh
+++ Calibre.sh
@@ -2,12 +2,14 @@
 
 (curl -sSLO https://raw.githubusercontent.com/thegreatestgiant/Scripts/main/Base.sh && chmod +x Base.sh && ./Base.sh -y) || apk update; apk add nano
 
+docker pull linuxserver/calibre:latest
+
 docker run -d \
   --name=calibre \
   --security-opt seccomp=unconfined \
   -e PUID=0 \
   -e PGID=0 \
-  -e TZ=Etc/UTC \
+  -e TZ=america/new_york \
   -p 8080:8080 \
   -p 8081:8081 \
   -v /calibre:/config \
