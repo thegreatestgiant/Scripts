dfd9d29305c14e548dfc2bcf01bebb341685ddb7
The Greatest Giant <seanalter1@gmail.com>
2023-04-14 09:56:20 -0400

    Update Base.sh

--- Base.sh
+++ Base.sh
@@ -16,14 +16,20 @@ setup_shell() {
     alias grep='grep --color=auto'
     alias unmount='fusermount -u Kavita'
     alias remount='fusermount -u Kavita; mount -a'
-    HISTTIMEFORMAT='%Y-%m-%d %T '" >> ~/.bashrc
+    HISTTIMEFORMAT='%Y-%m-%d %T '" | sudo tee -a /etc/bash.bashrc
 
     rm Base.sh
+
+    if ! id "sean" >/dev/null 2>&1; then
+        setup_better_user
+    fi
 
     exec bash
 }
 
-sudo apt update -y && sudo update-initramfs -u -y && sudo apt upgrade -y
+update() {
+    sudo apt update -y && sudo update-initramfs -u -y && sudo apt upgrade -y
+}
 
 install_all_packages() {
   echo "Installing all packages without prompting..."
@@ -49,6 +55,39 @@ install_selected_packages() {
   echo "Done!"
 }
 
+setup_better_user() {
+    echo "Creating new user..."
+    sudo useradd -m -s /bin/bash sean && echo "sean:ask your mom" | sudo chpasswd
+    sudo usermod -aG sudo sean
+    echo "Created"
+
+    echo "transfering ssh keys..."
+    echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFm6+eST03I30f2Llr8qnn40HiuH0F4w0wmUL4dOth1o sean@Home" | sudo tee /root/.ssh/authorized_keys > /dev/null
+    sudo mkdir /home/sean/.ssh
+    sudo cp /root/.ssh/authorized_keys /home/sean/.ssh/authorized_keys
+    sudo chown sean:sean -R /home/sean/.ssh
+    echo "transfered"
+
+    echo "sean   ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers.d/90-cloud-init-users > /dev/null
+
+    echo "flushing iptables"
+    sudo iptables --flush
+    sudo iptables -P INPUT ACCEPT
+    sudo iptables -P OUTPUT ACCEPT
+    sudo iptables -P FORWARD ACCEPT
+    sudo netfilter-persistent save -c
+    sudo DEBIAN_FRONTEND=noninteractive dpkg-reconfigure iptables-persistent
+
+    sudo apt remove snapd -y && sudo apt autoremove -y
+    sudo rm -rf /root/snap
+
+    echo "locking ubuntu account"
+    sudo usermod -L ubuntu
+    sudo passwd -l ubuntu
+    sudo chage -E0 ubuntu
+    sudo usermod -s /sbin/nologin ubuntu
+    echo "locked"
+}
 
 # Process command-line options
 while getopts ":hy" opt; do
@@ -60,6 +99,7 @@ while getopts ":hy" opt; do
       exit 0
       ;;
     y)
+      update
       install_all_packages
       setup_shell
       exit 0
@@ -71,14 +111,14 @@ while getopts ":hy" opt; do
   esac
 done
 
-sudo apt update && sudo apt upgrade -y
-
 echo -n "Do you want to install all packages? [Y/n] "
 read choice_all
 choice_all="${choice_all:-y}"
 if [[ "$choice_all" =~ ^[Yy]$ ]]; then
+  update
   install_all_packages
 else
+  update
   install_selected_packages
 fi
 
