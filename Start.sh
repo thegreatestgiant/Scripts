cbf78de5ad4b79116a45adbe21773ae37b74a7e2
The Greatest Giant <seanalter1@gmail.com>
2023-04-13 23:28:38 -0400

    Update Start.sh

--- Start.sh
+++ Start.sh
@@ -4,7 +4,8 @@ sudo useradd -m -s /bin/bash sean && echo "sean:ask your mom" | sudo chpasswd
 sudo usermod -aG sudo sean
 
 sudo mkdir /home/sean/.ssh
-sudo cp ~/.ssh/authorized_keys /home/sean/.ssh
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFm6+eST03I30f2Llr8qnn40HiuH0F4w0wmUL4dOth1o sean@Home" > ~/pub
+sudo mv ~/pub /home/sean/.ssh
 sudo chown sean:sean -R /home/sean/.ssh
 
 echo "sean   ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers.d/90-cloud-init-users > /dev/null
