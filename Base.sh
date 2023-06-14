c7ed2bea90e1ec23cd22906f93d895ee375ab0ef
The Greatest Giant <seanalter1@gmail.com>
2023-06-13 20:42:47 -0400

    Update Base.sh

--- Base.sh
+++ Base.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-PACKAGES=("nano" "net-tools" "docker" "docker.io" "docker-compose" "nautilus" "bat" "nginx" "sshfs" "zip" "unzip" "cmatrix" "tree")
+PACKAGES=("nano" "net-tools" "docker" "docker.io" "docker-compose" "nautilus" "bat" "sshfs" "zip" "tree")
 
 setup_shell() {
-    echo "
-    alias bat='batcat'
-    alias cmatrix='cmatrix -sb'
-    alias i='sudo apt install -y'
-    alias cd..='cd ..'
-    alias ip='curl icanhazip.com'
-    alias ls='ls --color=auto -h'
-    alias l='ls -CFh'
-    alias la='ls -Ah'
-    alias ll='ls -alhF'
-    alias speedtest='curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python3 -'
-    alias weather='curl wttr.in'
-    alias zupdate='sudo apt update && sudo apt upgrade -y'
-    alias grep='grep --color=auto'
-    alias unmount='fusermount -u Kavita'
-    alias remount='fusermount -u Kavita; mount -a'
-    HISTTIMEFORMAT='%Y-%m-%d %T '" | sudo tee -a /etc/bash.bashrc > /dev/null
+echo "
+alias bat='batcat'
+alias cmatrix='cmatrix -sb'
+alias i='sudo apt install -y'
+alias cd..='cd ..'
+alias ip='curl icanhazip.com'
+alias ls='ls --color=auto -h'
+alias l='ls -CFh'
+alias la='ls -Ah'
+alias ll='ls -alhF'
+alias speedtest='curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python3 -'
+alias weather='curl wttr.in'
+alias zupdate='sudo apt update && sudo apt upgrade -y'
+alias grep='grep --color=auto'
+alias unmount='fusermount -u Kavita'
+alias remount='fusermount -u Kavita; mount -a'
+HISTTIMEFORMAT='%Y-%m-%d %T '" | sudo tee -a /etc/bash.bashrc > /dev/null
 
     if ! id "sean" >/dev/null 2>&1; then
         setup_better_user
@@ -29,7 +29,7 @@ setup_shell() {
         sudo groupadd docker
     fi
     sudo usermod -aG docker sean
-
+    newgrp docker
     exec bash
 }
 
@@ -41,31 +41,7 @@ install_all_packages() {
   echo "Updating before installing all_packages"
   update
   echo "
-  Installing all packages without prompting...
-  "
-  sudo apt install -y "${PACKAGES[@]}"
-  echo "Done!"
-}
-
-install_selected_packages() {
-  echo "Please select which packages you don't want to install (enter the numbers separated by spaces):"
-  for i in "${!PACKAGES[@]}"; do
-    echo "[$i] ${PACKAGES[$i]}"
-  done
-
-  read -r -a choices
-  for choice in "${choices[@]}"; do
-    if [[ "$choice" =~ ^[0-9]+$ ]]; then
-      unset PACKAGES[$choice]
-    fi
-  done
-  
-  echo "
-  Updating b4 installing selected packages
-  "
-  update
-  echo "
-  Installing selected packages...
+  Installing all packages
   "
   sudo apt install -y "${PACKAGES[@]}"
   echo "Done!"
@@ -75,39 +51,7 @@ setup_better_user() {
     sudo bash -c "$(curl -sSL https://raw.githubusercontent.com/thegreatestgiant/Scripts/main/users.sh)"
 }
 
-# Process command-line options
-while getopts ":hy" opt; do
-  case $opt in
-    h)
-      echo "Usage: $0 [-y]"
-      echo "  -y: automatically install all packages without prompting"
-      echo "  -h: display this help message"
-      exit 0
-      ;;
-    y)
-      install_all_packages
-      setup_shell
-      exit 0
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      exit 1
-      ;;
-  esac
-done
-
-echo -n "Do you want to install all packages? [Y/n] "
-read choice_all
-choice_all="${choice_all:-y}"
-if [[ "$choice_all" =~ ^[Yy]$ ]]; then
-  install_all_packages
-else
-  install_selected_packages
-fi
-
-if test -f Base.sh;then
-    rm Base.sh
-fi
+install_all_packages
 
 setup_shell
 
