04431a4439c70387f94e3f565458b81b58985e19
The Greatest Giant <seanalter1@gmail.com>
2023-08-24 11:40:45 -0400

    Update Base.sh

--- Base.sh
+++ Base.sh
@@ -47,6 +47,11 @@ install_all_packages() {
   Installing all packages
   "
   sudo apt install --no-install-recommends -y "${PACKAGES[@]}"
+  # node
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+  source ~/.bashrc
+  nvm install 18
+
   echo "Done!"
 }
 
