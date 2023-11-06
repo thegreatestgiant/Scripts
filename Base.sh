c1d19e4d599519808b381a4be60094f91ca2b869
The Greatest Giant <seanalter1@gmail.com>
2023-11-05 21:12:04 -0500

    Update Base.sh

--- Base.sh
+++ Base.sh
@@ -72,11 +72,23 @@ install_all_packages() {
   "
   remove_installed_from_list
   apt install --no-install-recommends -y "${PACKAGES[@]}"
+  install_neovim
   echo "Done!"
 }
 
 setup_better_user() {
     bash -c "$(curl -sSL https://raw.githubusercontent.com/thegreatestgiant/Scripts/main/users.sh)"
+}
+
+install_neovim() {
+    curl -Lo /usr/bin/nvim https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+    chmod u+x /usr/bin/nvim
+    add-apt-repository universe
+    apt install -y libfuse2 fuse libfuse2
+    modprobe fuse
+    groupadd fuse
+    user="$(whoami)"
+    usermod -a -G fuse $user
 }
 
 install_all_packages
