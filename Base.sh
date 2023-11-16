780b6d6723b3f1702315cb81f4104827d5ea8507
The Greatest Giant <seanalter1@gmail.com>
2023-11-16 14:48:22 -0500

    Update Base.sh
    add debian repo's for gh

--- Base.sh
+++ Base.sh
@@ -51,7 +51,11 @@ EOL
 
 update() {
     DEBIAN_FRONTEND=noninteractive apt update && DEBIAN_FRONTEND=noninteractive apt upgrade -y
-}
+    type -p curl >/dev/null || (apt install curl -y)
+    curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+    chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+    }
 
 remove_installed_from_list() {
     local installed_packages=()
